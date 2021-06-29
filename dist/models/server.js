ef0037f7f7a44937f47e509e6cd9d7903929bd97
Cesar Candia <cesarcandia@iMac-de-Cesar.local>
2021-06-29 13:28:26 -0500

    c

--- dist/models/server.js
+++ dist/models/server.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 // Routes
 const user_1 = __importDefault(require("../routes/user"));
 class Server {
@@ -13,14 +14,26 @@ class Server {
         };
         this.app = express_1.default();
         this.port = process.env.PORT || '8000';
+        // Inicializar middlewares.
+        this.middlewares();
         // Inicializar routes.
         this.routes();
+    }
+    // Conexión a la Base de Datos
+    // Middlewares
+    middlewares() {
+        // Cors
+        this.app.use(cors_1.default());
+        // Lectura del body
+        this.app.use(express_1.default.json());
+        // Carpeta publica
+        this.app.use(express_1.default.static('public'));
     }
     // Routes
     routes() {
         this.app.use(this.paths.users, user_1.default);
     }
-    // Levantar el servidor.
+    // Inicializar el servidor.
     listen() {
         this.app.listen(this.port, () => {
             console.log('Servidor corriendo en puerto' + this.port);
--- models/server.ts
+++ models/server.ts
@@ -1,4 +1,5 @@
 import express, { Application } from "express";
+import cors from "cors";
 
 // Routes
 import UserRoute from "../routes/user";
@@ -14,9 +15,26 @@ class Server {
     constructor() {
         this.app = express();
         this.port = process.env.PORT || '8000';
+        
+        // Inicializar middlewares.
+        this.middlewares();
 
         // Inicializar routes.
         this.routes();
+    }
+
+    // Conexión a la Base de Datos
+
+    // Middlewares
+    middlewares() {
+        // Cors
+        this.app.use(cors());
+
+        // Lectura del body
+        this.app.use(express.json());
+
+        // Carpeta publica
+        this.app.use(express.static('public'));
     }
 
     // Routes
@@ -24,13 +42,13 @@ class Server {
         this.app.use(this.paths.users, UserRoute);
     }
 
-    // Levantar el servidor.
+    // Inicializar el servidor.
     listen() {
         this.app.listen(this.port, () =>{
             console.log('Servidor corriendo en puerto' + this.port);
-            
         });
     }
+
 }
 
 export default Server;
