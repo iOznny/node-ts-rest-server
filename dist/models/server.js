d585ce9330c2cd83e3b3f7b2cf659e9bc4444af4
Cesar Candia <cesarcandia@iMac-de-Cesar.local>
2021-06-29 12:59:14 -0500

    c

--- dist/models/server.js
+++ dist/models/server.js
@@ -4,12 +4,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+// Routes
+const user_1 = __importDefault(require("../routes/user"));
 class Server {
     constructor() {
+        this.paths = {
+            users: '/api/users'
+        };
         this.app = express_1.default();
         this.port = process.env.PORT || '8000';
+        // Inicializar routes.
+        this.routes();
     }
-    // levantar el servidor.
+    // Routes
+    routes() {
+        this.app.use(this.paths.users, user_1.default);
+    }
+    // Levantar el servidor.
     listen() {
         this.app.listen(this.port, () => {
             console.log('Servidor corriendo en puerto' + this.port);
--- models/server.ts
+++ models/server.ts
@@ -1,16 +1,30 @@
 import express, { Application } from "express";
 
+// Routes
+import UserRoute from "../routes/user";
+
 class Server {
 
     private app: Application;
     private port: string;
+    private paths = {
+        users: '/api/users'
+    };
 
     constructor() {
         this.app = express();
         this.port = process.env.PORT || '8000';
+
+        // Inicializar routes.
+        this.routes();
     }
 
-    // levantar el servidor.
+    // Routes
+    routes() {
+        this.app.use(this.paths.users, UserRoute);
+    }
+
+    // Levantar el servidor.
     listen() {
         this.app.listen(this.port, () =>{
             console.log('Servidor corriendo en puerto' + this.port);
