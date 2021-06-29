424c64d999af41c980e0cb07f92cede3834fa344
Cesar Candia <cesarcandia@iMac-de-Cesar.local>
2021-06-29 12:42:18 -0500

    c

--- dist/controllers/user.js
+++ dist/controllers/user.js
@@ -1,4 +1,53 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.deleteUser = exports.putUser = exports.postUser = exports.getUser = exports.getUsers = void 0;
+// Obtener Usuarios.
 const getUsers = (req, res) => {
+    res.json({
+        method: 'GET',
+        message: 'Users'
+    });
 };
+exports.getUsers = getUsers;
+// Obtener Usuario.
+const getUser = (req, res) => {
+    const { id } = req.params;
+    res.json({
+        method: 'GET',
+        message: 'User',
+        id
+    });
+};
+exports.getUser = getUser;
+// Crear Usuario.
+const postUser = (req, res) => {
+    const { body } = req;
+    res.json({
+        method: 'POST',
+        message: 'User',
+        body
+    });
+};
+exports.postUser = postUser;
+// Actualizar Usuario.
+const putUser = (req, res) => {
+    const { id } = req.params;
+    const { body } = req;
+    res.json({
+        method: 'PUT',
+        message: 'User',
+        id,
+        body
+    });
+};
+exports.putUser = putUser;
+// Eliminar Usuario.
+const deleteUser = (req, res) => {
+    const { id } = req.params;
+    res.json({
+        method: 'DELETE',
+        message: 'User',
+        id,
+    });
+};
+exports.deleteUser = deleteUser;
